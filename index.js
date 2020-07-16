53fed187f8778dd8b6162bde914bb4a89b7c7a2a
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-07-15 22:49:12 -0400

    adding new stuff

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const server = require('./server.js');
 
-const PORT = 4000;
-server.listen(PORT, () =>{
-    console.log(`\n*** Server running on http://localhost: ${PORT} ***\n`);
+const port = process.env.PORT;
+server.listen(PORT, () => {
+    console.log(`\n*** Server running on http://localhost:${PORT} ***\n`);
 });
